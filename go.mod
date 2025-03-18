9999aacab21b614c278131c73af95ecc605f9238
Ronald Garcia <ronaldag58@gmail.com>
2025-03-17 21:57:39 -0300

    create project

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/RonaldAG/go-microservice
+
+go 1.24.1
---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/RonaldAG/go-microservice/handlers"
+)
+
+func main() {
+	l := log.New(os.Stdout, "product-api", log.Default().Flags())
+	hh := handlers.NewHello(l)
+	gh := handlers.NewGoodbye(l)
+
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
+	sm.Handle("/goodbye", gh)
+	
+	
+	server := &http.Server{
+		Addr: ":8080",
+		Handler: sm,
+		IdleTimeout: 120 *time.Second,
+		ReadTimeout: 1 *time.Second,
+		WriteTimeout: 1 *time.Second,
+	}
+	go func() {
+		err :=	server.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	sig := <- sigChan
+	l.Println("Received terminate, graceful shutdown", sig)
+
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	server.Shutdown(tc)
+}
